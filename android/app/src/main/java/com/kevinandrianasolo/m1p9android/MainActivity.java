92aa9a2bdb6a57b42efef0d835e0ec5b4271c885
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-05-12 12:36:05 +0300

    Android Project

---
+++ android/app/src/main/java/com/kevinandrianasolo/m1p9android/MainActivity.java
@@ -0,0 +1,78 @@
+package com.kevinandrianasolo.m1p9android;
+
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.view.View;
+import android.view.Menu;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.kevinandrianasolo.m1p9android.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+
+        // Hide default ActionBar
+        getSupportActionBar().hide();
+
+        this.drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(this.drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        this.initMenu();
+    }
+
+    public void initMenu(){
+        findViewById(R.id.toolbar_menu_launcher).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // To programatically Show and Hide Menu on the left
+                MainActivity.this.drawer.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
---
+++ android/app/src/main/java/com/kevinandrianasolo/m1p9android/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.kevinandrianasolo.m1p9android;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar!=null) actionBar.hide();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
