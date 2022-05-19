370400f6f013d08621f1e65d7d95717cb4428d8e
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-05-19 19:08:57 +0300

    Try You tube video

--- android/app/src/main/java/com/kevinandrianasolo/m1p9android/ui/category_courses/CategoryCoursesFragment.java
+++ android/app/src/main/java/com/kevinandrianasolo/m1p9android/ui/category_courses/CategoryCoursesFragment.java
@@ -60,7 +60,6 @@ public class CategoryCoursesFragment extends Fragment {
         courseList.add(new Course("Trouver la parfaite boite avec l'Escouade des monstres", "Avec ludikids, les enfants seront exposés aux nombres, aux formes, aux calculs, aux concepts d’additions et de soustractions.", "http://192.168.1.101:3000/public/courses/test.mkv", "https://i.picsum.photos/id/961/200/200.jpg?hmac=gHwrXvhjUL97oGKmAYQn508wdQ_V5sE9P64erzR-Ork"));
         courseList.add(new Course("Jusqu’à 10 avec les Numberblocks", "Pas de description", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", "https://i.picsum.photos/id/593/200/200.jpg?hmac=E26lTUTkzs_AeuWXrkT-kFTudfYDTVCjgKVE_HDzRmk"));
         courseList.add(new Course("Le soleil, une super-étoile", "Avec ludikids, les enfants acquerront de la connaissance sur les sciences, la nature, la technologie, le monde environnant et sur la façon dont les choses fonctionnent.", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4", "https://i.picsum.photos/id/352/200/200.jpg?hmac=HPgFQ0Sto_7261sbYIaRW0-z2Jq0-C92RSt0vkdC6Uc"));
-
         ListView coursesListView = view.findViewById(R.id.courses_list_view);
         coursesListView.setAdapter(new CourseAdapter(view.getContext(), courseList));
 
@@ -91,7 +90,6 @@ public class CategoryCoursesFragment extends Fragment {
                 List<Course> courseListFiltered = new ArrayList<Course>();
                 courseListFiltered.add(new Course("Trouver la parfaite boite avec l'Escouade des monstres", "Avec ludikids, les enfants seront exposés aux nombres, aux formes, aux calculs, aux concepts d’additions et de soustractions.", "https://ex1.o7planning.com/_testdatas_/mov_bbb.mp4", "https://i.picsum.photos/id/961/200/200.jpg?hmac=gHwrXvhjUL97oGKmAYQn508wdQ_V5sE9P64erzR-Ork"));
                 courseListFiltered.add(new Course("Le soleil, une super-étoile", "Avec ludikids, les enfants acquerront de la connaissance sur les sciences, la nature, la technologie, le monde environnant et sur la façon dont les choses fonctionnent.", "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4", "https://i.picsum.photos/id/352/200/200.jpg?hmac=HPgFQ0Sto_7261sbYIaRW0-z2Jq0-C92RSt0vkdC6Uc"));
-
                 coursesListView.setAdapter(new CourseAdapter(view.getContext(), courseListFiltered));
 
                 if(courseListFiltered.size() == 0) searchCourseMessage.setVisibility(View.VISIBLE);//.setVisibility(View.VISIBLE);
--- android/app/src/main/java/com/kevinandrianasolo/m1p9android/ui/course/CourseFragment.java
+++ android/app/src/main/java/com/kevinandrianasolo/m1p9android/ui/course/CourseFragment.java
@@ -13,11 +13,16 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.kevinandrianasolo.m1p9android.R;
 import com.kevinandrianasolo.m1p9android.models.Course;
 import com.kevinandrianasolo.m1p9android.utils.MediaUtils;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class CourseFragment extends Fragment {
 
@@ -56,6 +61,30 @@ public class CourseFragment extends Fragment {
          */
         VideoView videoView = view.findViewById(R.id.course_video);
         MediaUtils.loadVideo(view.getContext(), videoView, currentCourse.getSrc(), true, progDialog);
+        /*
+        // For you tube videos
+        YouTubePlayerView youTubePlayerView = view.findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+
+        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+            @Override
+            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
+                String videoId = currentCourse.getSrc();
+                youTubePlayer.loadVideo(videoId, 0);
+            }
+
+            @Override
+            public void onError(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlayerError error){
+
+                Toast.makeText(view.getContext(), "Une erreur est survenue lors du chargement de la vidéo : "+error.toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onVideoId(@NonNull YouTubePlayer youTubePlayer, String videoId){
+                progDialog.dismiss();
+                Toast.makeText(view.getContext(), "La vidéo est chargée", Toast.LENGTH_SHORT).show();
+            }
+        });*/
 
         /**
          * Setting views variable Course.title and Course.description whith the fragment parameters
