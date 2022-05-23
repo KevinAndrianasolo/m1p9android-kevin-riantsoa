e321352a5a8d110e7ffc7aa35a564929c663c672
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-05-23 15:44:39 +0300

    Update PGconnect.js

--- utils/PGconnect.js
+++ utils/PGconnect.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-const { Pool } = require('pg');
+const { Client, Pool } = require('pg');
 var pg = require("pg");
 
-const pool = new pg.Client({
+const pool = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
