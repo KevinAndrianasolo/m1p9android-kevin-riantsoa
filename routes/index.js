b01808bf52dbb1a5425e93fada2415fa52b2874e
Kevin Andrianasolo <kevin.andrianasolo.lala@gmail.com>
2022-05-23 14:51:53 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const path = require('path');
 var appDir = path.resolve(__dirname, '..');
 
 //import route 
-var company = require('./CompanyRoute.js');
-var asset = require('./AssetRoute.js');
-var courseTheme = require('./CourseThemeRoute.js');
-var course = require('./CourseRoute.js');
-var user = require('./userRoute.js');
+var company = require('./CompanyRoute');
+var asset = require('./AssetRoute');
+var courseTheme = require('./CourseThemeRoute');
+var course = require('./CourseRoute');
+var user = require('./UserRoute');
 
 //all route
 router.use('/api/company', company);
